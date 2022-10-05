2dd17de0c31aa6e2691e1fa0944d348584082da8
Tatyana Larina <jerrymurr@ya.ru>
2022-10-05 23:26:10 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int cost = 10000;
+        int mile = 100;
+
+        int miles = cost / mile;
+
+        System.out.println(miles);
+
     }
 }
